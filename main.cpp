5b3cf19c785d7de4200dff8e0e4bbd9f18c9b20b
bhiaibogf <bhiaibogf@outlook.com>
2022-01-05 16:33:15 +0800

    set model

--- main.cpp
+++ main.cpp
@@ -9,17 +9,32 @@
 int main(int argc, const char **argv) {
     Rasterizer rasterizer(512, 512);
 
-    Eigen::Vector3f eye_pos = {0, 0, 10};
+    Eigen::Vector3f eye_pos = {0, 0, 4};
     Camera camera;
 
-    Mesh mesh = obj_loader::LoadObj("objs/spot/spot_triangulated_good.obj");
-    // Mesh mesh = Mesh({{2,   0,   -2},
-    //                   {0,   2,   -2},
-    //                   {-2,  0,   -2},
-    //                   {3.5, -1,  -5},
-    //                   {2.5, 1.5, -5},
-    //                   {-1,  0.5, -5}}, {{0, 1, 2},
-    //                                     {3, 4, 5}});
+    // spot
+    // Mesh mesh = obj_loader::LoadObj("objs/spot/spot_triangulated_good.obj");
+    // mesh.set_model(trans_former::GetTranslation() *
+    //                trans_former::GetRotation(140.f) *
+    //                trans_former::GetScale(1.5, 1.5, 1.5));
+
+    // bunny
+    // Mesh mesh = obj_loader::LoadObj("objs/bunny/bunny.obj");
+    // mesh.set_model(trans_former::GetTranslation(0, -1, 0) *
+    //                trans_former::GetRotation(15) *
+    //                trans_former::GetScale(10, 10, 10));
+
+    // two triangles
+    Mesh mesh = Mesh({{2,   0,   -2},
+                      {0,   2,   -2},
+                      {-2,  0,   -2},
+                      {3.5, -1,  -5},
+                      {2.5, 1.5, -5},
+                      {-1,  0.5, -5}},
+                     {{0, 1, 2},
+                      {3, 4, 5}});
+    mesh.set_model(trans_former::GetScale(0.5, 0.5, 0.2));
+
     // std::cout << mesh;
 
     int key = 0;
@@ -27,10 +42,8 @@ int main(int argc, const char **argv) {
     while (key != 27) {
         rasterizer.Clear();
 
-        mesh.set_model(trans_former::GetTranslation() * trans_former::GetRotation() * trans_former::GetScale());
-
         camera.set_view(eye_pos);
-        camera.set_projection(45, 1, 0.1, 50);
+        camera.set_projection(45, 1, 0.1, 6);
 
         rasterizer.Draw(camera, mesh);
         rasterizer.Show();
--- model/transformer.h
+++ model/transformer.h
@@ -8,9 +8,9 @@
 #include <eigen3/Eigen/Eigen>
 
 namespace trans_former {
-    Eigen::Matrix4f GetScale(float scale_x = 2.5, float scale_y = 2.5, float scale_z = 2.5);
+    Eigen::Matrix4f GetScale(float scale_x = 0.f, float scale_y = 0.f, float scale_z = 0.f);
 
-    Eigen::Matrix4f GetRotation(float angle = 140.0);
+    Eigen::Matrix4f GetRotation(float angle = 0.f);
 
     Eigen::Matrix4f GetTranslation(float translation_x = 0, float translation_y = 0, float translation_z = 0);
 }
