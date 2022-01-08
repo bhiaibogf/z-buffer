e14813de0fef34a2b5139432a05b576f8a1d8ac8
bhiaibogf <bhiaibogf@outlook.com>
2022-01-08 19:44:24 +0800

    switch between two z-buffers

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,24 @@
 #include "rasterizer/vanilla.h"
 #include "rasterizer/scan_line.h"
 
-int main(int argc, const char **argv) {
-    Vanilla rasterizer(512, 512);
-    // ScanLine rasterizer(512, 512);
+int main() {
+    Vanilla vanilla(512, 512);
+    ScanLine scan_line(512, 512);
 
     Eigen::Vector4f eye_pos = {0, 0, 4, 1};
     Camera camera;
-
-    // spot
-    // Mesh mesh = obj_loader::LoadObj("model/spot/spot_triangulated_good.obj");
-    // mesh.set_model(trans_former::GetTranslation() *
-    //                trans_former::GetRotation(140.f) *
-    //                trans_former::GetScale(1.5, 1.5, 1.5));
 
     // bunny
     Mesh mesh = obj_loader::LoadObj("model/bunny/bunny.obj");
     mesh.set_model(trans_former::GetTranslation(0, -1, 0) *
                    trans_former::GetRotation(15) *
                    trans_former::GetScale(10, 10, 10));
+
+    // spot
+    // Mesh mesh = obj_loader::LoadObj("model/spot/spot_triangulated_good.obj");
+    // mesh.set_model(trans_former::GetTranslation() *
+    //                trans_former::GetRotation(140.f) *
+    //                trans_former::GetScale(1.5, 1.5, 1.5));
 
     // cube
     // Mesh mesh = obj_loader::LoadObj("model/cube/cube.obj");
@@ -49,17 +49,25 @@ int main(int argc, const char **argv) {
     int key = 0;
     int frame_count = 0;
     float angle_y = 0, z_translation = 0;
-    bool show_depth = true;
+    bool show_depth = true, use_scan_line = true;
     while (key != 27) {
-        rasterizer.Clear();
+        vanilla.Clear();
+        scan_line.Clear();
 
         camera.set_view((trans_former::GetRotation(angle_y) *
                          trans_former::GetTranslation(0, 0, z_translation) *
                          eye_pos).head<3>());
         camera.set_projection(45, 1, 0.1, 6);
 
-        rasterizer.Draw(camera, mesh);
-        rasterizer.Show(show_depth);
+        if (use_scan_line) {
+            scan_line.Draw(camera, mesh);
+            scan_line.Show(show_depth);
+            puts("draw using scan line");
+        } else {
+            vanilla.Draw(camera, mesh);
+            vanilla.Show(show_depth);
+            puts("draw using normal z-buffer");
+        }
         key = cv::waitKey(10);
 
         std::cout << "frame count: " << frame_count++ << '\n';
@@ -82,10 +90,17 @@ int main(int argc, const char **argv) {
                 z_translation = 0;
                 break;
             case 'v':
-                rasterizer.Save(show_depth);
+                if (use_scan_line) {
+                    scan_line.Save(show_depth);
+                } else {
+                    vanilla.Save(show_depth);
+                }
                 break;
             case 'c':
                 show_depth = !show_depth;
+                break;
+            case 'l':
+                use_scan_line = !use_scan_line;
                 break;
             default:
                 break;
